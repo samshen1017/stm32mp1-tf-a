2704dbb81cc035e8e82afb04a6086f9caf7382dc
墨鸿苍穹 <samshen1017@sina.com>
2023-03-23 15:31:16 +0800

    增加一键编译脚本

--- make_TF-A.sh
+++ make_TF-A.sh
@@ -6,11 +6,20 @@ ENV_SET=/home/isaac/toolchain/st/stm32mp1/4.0.4-openstlinux-5.15-yocto-kirkstone
 cd $TF_A_DIR
 
 #设置编译器
-$ source $ENV_SET
+source $ENV_SET
 
 #设置FIP目录
-$ export FIP_DEPLOYDIR_ROOT=$PWD/../FIP_artifacts
+export FIP_DEPLOYDIR_ROOT=$PWD/../FIP_artifacts
 
-#编译
-$ make -f $PWD/../Makefile.sdk DEPLOYDIR=$FIP_DEPLOYDIR_ROOT/arm-trusted-firmware -j12 all
-$ make -f $PWD/../Makefile.sdk -j12 all
+if [ $1 == "all" ]; then
+    #编译所有目标板
+    make -f $PWD/../Makefile.sdk DEPLOYDIR=$FIP_DEPLOYDIR_ROOT/arm-trusted-firmware -j12 all
+
+elif [ $1 == "astor" ]; then
+    #编译定制板
+    make -f $PWD/../Makefile.sdk DEPLOYDIR=$FIP_DEPLOYDIR_ROOT/arm-trusted-firmware TF_A_DEVICETREE=stm32mp157c-astro -j12 all
+
+elif [ $1 == "clean" ]; then
+    make -f $PWD/../Makefile.sdk clean
+fi
+
