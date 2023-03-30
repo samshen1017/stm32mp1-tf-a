7a8534d92cb65feec775ac32d2eabe2c180632c8
samshen1017 <samshen1017@sina.com>
2023-03-30 18:48:02 +0800

    更新astro设备树

--- make_TF-A.sh
+++ make_TF-A.sh
@@ -10,11 +10,18 @@ source $ENV_SET
 
 if [ $1 == "debug" ]; then
     unset -v CFLAGS LDFLAGS
-    make ARCH=aarch32 ARM_ARCH_MAJOR=7 DTB_FILE_NAME=stm32mp157c-astro.dtb STM32MP_SDMMC=1 STM32MP_EMMC=1 DEBUG=1
+    make CROSS_COMPILE=arm-ostl-linux-gnueabi- PLAT=stm32mp1 ARCH=aarch32 ARM_ARCH_MAJOR=7 DTB_FILE_NAME=stm32mp157c-astro.dtb STM32MP_SDMMC=1 STM32MP_EMMC=1 DEBUG=1
 
 elif [ $1 == "bl32" ]; then
     unset -v CFLAGS LDFLAGS
-    make PLAT=stm32mp1 ARCH=aarch32 ARM_ARCH_MAJOR=7 AARCH32_SP=sp_min DTB_FILE_NAME=stm32mp157c-astro.dtb DEBUG=1 bl32 dtbs
+    make CROSS_COMPILE=arm-ostl-linux-gnueabi- PLAT=stm32mp1 ARCH=aarch32 ARM_ARCH_MAJOR=7 AARCH32_SP=sp_min DTB_FILE_NAME=stm32mp157c-astro.dtb DEBUG=1 bl32 dtbs
+
+elif [ $1 == "copy" ]; then
+    mkdir -p ../../image
+    cp ./build/stm32mp1/debug/tf-a-stm32mp157c-astro.stm32 ../../image/
+    cp ./build/stm32mp1/debug/bl32.bin ../../image/
+    cp ./build/stm32mp1/debug/fdts/stm32mp157c-astro-fw-config.dtb ../../image/
+    cp ./build/stm32mp1/debug/fdts/stm32mp157c-astro-bl32.dtb ../../image/
 
 elif [ $1 == "clean" ]; then
     make distclean
